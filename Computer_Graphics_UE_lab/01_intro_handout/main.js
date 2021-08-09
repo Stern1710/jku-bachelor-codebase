0953d1039344ec137471bf8ab48f4038b6295e96
Kathi Sternbauer <k11812499@students.jku.at>
2021-08-09 08:40:26 +0200

    Add computer graphics UE lab and project

---
+++ Computer_Graphics_UE_lab/01_intro_handout/main.js
@@ -0,0 +1,158 @@
+/**
+ * the OpenGL context
+ * @type {WebGLRenderingContext}
+ */
+var gl = null;
+
+/**
+ * initializes OpenGL context, compile shader, and load buffers
+ */
+function init(resources) {
+  //create a GL context
+  gl = createContext(800 /*width*/, 450 /*height*/);
+  
+  //TODO initialize shader program and buffers...
+  //in WebGL/OpenGL3 we have to create and use our own shaders for the programmable pipeline
+  //create the shader program
+  shaderProgram = createProgram(gl, resources.vs, resources.fs);
+
+  //create a buffer and put a single clipspace rectangle in it (2 triangles)
+  buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  //we need typed arrays
+  const arr = new Float32Array([
+    -1.0, 1.0,
+    -1.0, 0.67,
+    1.0, 1.0,
+    -1.0, 0.67,
+    1.0, 1.0,
+    1.0, 0.67,
+    -1.0, 0.67,
+    -1.0, 0.33,
+    1.0, 0.67,
+    -1.0, 0.33,
+    1.0, 0.67,
+    1.0, 0.33,
+    -1.0, 0.33,
+    -1.0, 0.0,
+    1.0, 0.33,
+    -1.0, 0.0,
+    1.0, 0.33,
+    1.0, 0.0,
+    -1.0, 0.0,
+    -1.0, -0.33,
+    1.0, 0.0,
+    -1.0, -0.33,
+    1.0, 0.0,
+    1.0, -0.33,
+    -1.0, -0.33,
+    -1.0, -0.67,
+    1.0, -0.33,
+    -1.0, -0.67,
+    1.0, -0.33,
+    1.0, -0.67,
+    -1.0, -0.67,
+    -1.0, -1.0,
+    1.0, -0.67,
+    -1.0, -1.0,
+    1.0, -0.67,
+    1.0, -1.0
+  ]);
+  //copy data to GPU
+  gl.bufferData(gl.ARRAY_BUFFER, arr, gl.STATIC_DRAW);
+
+  colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  const colors = new Float32Array([
+    1, 0, 0.09, 1,
+    1, 0, 0.09, 1,
+    1, 0, 0.09, 1,
+    1, 0, 0.09, 1,
+    1, 0, 0.09, 1,
+    1, 0, 0.09, 1,
+    1, 0.64, 0.17, 1,
+    1, 0.64, 0.17, 1,
+    1, 0.64, 0.17, 1,
+    1, 0.64, 0.17, 1,
+    1, 0.64, 0.17, 1,
+    1, 0.64, 0.17, 1,
+    1, 1, 0, 1,
+    1, 1, 0, 1,
+    1, 1, 0, 1,
+    1, 1, 0, 1,
+    1, 1, 0, 1,
+    1, 1, 0, 1,
+    0, 0.5, 0.09, 1,
+    0, 0.5, 0.09, 1,
+    0, 0.5, 0.09, 1,
+    0, 0.5, 0.09, 1,
+    0, 0.5, 0.09, 1,
+    0, 0.5, 0.09, 1,
+    0, 0, 0.97, 1,
+    0, 0, 0.97, 1,
+    0, 0, 0.97, 1,
+    0, 0, 0.97, 1,
+    0, 0, 0.97, 1,
+    0, 0, 0.97, 1,
+    0.52, 0, 0.49, 1,
+    0.52, 0, 0.49, 1,
+    0.52, 0, 0.49, 1,
+    0.52, 0, 0.49, 1,
+    0.52, 0, 0.49, 1,
+    0.52, 0, 0.49, 1
+  ]);
+  gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+}
+
+/**
+ * render one frame
+ */
+function render() {
+  //specify the clear color
+  gl.clearColor(0.9, 0.9, 0.9, 1.0);
+  //clear the buffer
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  //TODO render scene
+  //activate shader program that you have initialized before
+  gl.useProgram(shaderProgram);
+
+  var userColor = {r: 0.7, g: 0.3, b: 0.4};
+  gl.uniform3f(gl.getUniformLocation(shaderProgram, 'u_usercolor'), userColor.r, userColor.g, userColor.b);
+
+  //we look up the internal location after compilation of the shader program given the name of the attribute
+  const positionLocation = gl.getAttribLocation(shaderProgram, 'a_position');
+
+  //enable this vertex attribute
+  gl.enableVertexAttribArray(positionLocation);
+  //use the currently bound buffer for this location
+  //each element is a FLOAT with 2 components
+  //2 .. number of components
+  //float ... type
+  //false ... the array should not be normalized
+  //stride / offset ... in case you are interleaving different attribute
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+  const colorLocation = gl.getAttribLocation(shaderProgram, 'a_color');
+  gl.enableVertexAttribArray(colorLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.vertexAttribPointer(colorLocation, 4, gl.FLOAT, false, 0, 0);
+
+  // draw the bound data as 6 vertices = 2 triangles starting at index 0
+  gl.drawArrays(gl.TRIANGLES, 0, 36);
+
+  //request another call as soon as possible
+  //requestAnimationFrame(render)
+
+}
+
+loadResources({
+  //list of all resources that should be loaded as key: path
+  vs: 'shader/simple.vs.glsl',
+  fs: 'shader/simple.fs.glsl'
+}).then(function (resources /*loaded resources*/) {
+  init(resources);
+  //render one frame
+  render();
+});
---
+++ Computer_Graphics_UE_lab/01_intro_handout/shader/simple.fs.glsl
@@ -0,0 +1,21 @@
+/**
+ * as simple fragment shader just setting the provided color as fragment color
+ * Created by Samuel Gratzl on 08.02.2016.
+ */
+
+//need to specify how "precise" float should be
+precision mediump float;
+
+uniform vec3 u_usercolor;
+
+//interpolate argument between vertex and fragment shader
+varying vec4 v_color;
+
+//entry point again
+void main() {
+  //gl_FragColor ... magic output variable containing
+  //                 the final 4D color of the fragment
+  // gl_FragColor = vec4(u_usercolor, 1);
+
+  gl_FragColor = v_color;
+}
